552b4c517ccde588e5f32beaa88f0023110f0939
Gilberto Avalos <gavalos@itexico.com>
2014-03-27 12:36:09 -0600

    Added initial files

--- app.js
+++ app.js
@@ -7,16 +7,15 @@
 
 var express = require('express'),
 routes      = require('./routes'),
-http        = require('http');
+config      = require('./config');
 
 var app     = express();
-var server  = app.listen(3000);
+var server  = app.listen(config.port);
 var io      = require('socket.io').listen(server);
  
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
@@ -28,6 +27,4 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
  
-app.get('/', routes.index);
- 
-console.log("Express server listening on port 3000");
+console.log("Express server listening on port " + config.port);
