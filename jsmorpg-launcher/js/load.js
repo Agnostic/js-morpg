a42344f4a9e3883c54ab404a064ff5d9d8e9556d
Gilberto Avalos <gavalos@itexico.com>
2014-04-30 17:35:11 -0500

    Removed launcher

--- jsmorpg-launcher/js/load.js
+++
@@ -1,5 +0,0 @@
-CocoonJS.App.onLoadInTheWebViewSucceed.addEventListener(function(pageURL) {
-  CocoonJS.App.showTheWebView();
-});
-
-CocoonJS.App.loadInTheWebView('http://jsmorpg.noip.me:3000/play');
