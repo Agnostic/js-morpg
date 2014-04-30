b1a1f21b03447be7facaaebeb44d387fe3b55ffc
Gilberto Avalos <gavalos@itexico.com>
2014-04-30 17:11:51 -0500

    Added jsmorpg launcher with webview for cocoonJS

---
+++ jsmorpg-launcher/js/load.js
@@ -0,0 +1,5 @@
+CocoonJS.App.onLoadInTheWebViewSucceed.addEventListener(function(pageURL) {
+  CocoonJS.App.showTheWebView();
+});
+
+CocoonJS.App.loadInTheWebView('http://jsmorpg.noip.me:3000/play');
